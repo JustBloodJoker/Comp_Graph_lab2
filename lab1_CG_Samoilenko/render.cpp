1f7d24327723c5dab877692c528a63abbb3e06a7
JustBloodJoker <tartul21@gmail.com>
2024-10-12 12:41:21 +0300

    lab2-UPD

--- lab1_CG_Samoilenko/render.cpp
+++ lab1_CG_Samoilenko/render.cpp
@@ -1,7 +1,7 @@
 #include "render.h"
 
 render::render()
-    : WinWindow(L"Lab1_ComputerGraphicsSamoilenko", 800, 600, false)
+    : WinWindow(L"Lab2_ComputerGraphicsSamoilenko", 800, 600, false)
 {
 }
 
@@ -13,16 +13,17 @@ void render::UserAfterD2DInit()
 
 void render::UserD2DLoop()
 {
-    rtv->Clear(D2D1::ColorF(D2D1::ColorF::White));
+    rtv->Clear( D2D1::ColorF(D2D1::ColorF::White) );
+    SetMainBrushColor(D2D1::ColorF::Red);
     
-    
-    //drawSurnameBresenham();
-    if(drawHandler) drawHandler();
-
-   
-
-
-
+    std::vector<D2D1_POINT_2F> curve;
+    for (int i = 0; i <= 1000; i++) {
+        float t = (static_cast<float>(i) / 1000);
+        curve.push_back(ComputePoint(t));
+    }
+    for (int i = 1; i < curve.size(); ++i) {
+        rtv->DrawLine(curve[i - 1], curve[i], brush);
+    }
 }
 
 void render::UserD2DClose()
@@ -31,292 +32,109 @@ void render::UserD2DClose()
 
 void render::ChildKeyPressed(WPARAM param)
 {
-    if (param == VK_F1) {
-        drawHandler = [this]() { drawSurnameDDA(); };
+    if (param == VK_UP) {
+        ScaleFactor *= 1.1;
+        Scale(ScaleFactor);
     }
-    else if (param == VK_F2) {
-        drawHandler = [this]() { drawSurnameWu(); };
+    else if (param == VK_DOWN) {
+        ScaleFactor /= 1.1;
+        Scale(ScaleFactor);
+    }
+    else if (param == VK_F1) {
+        Rotate(0.1f);
     }  
+    else if (param == VK_F2) {
+        Rotate(-0.1f);
+    }
     else if (param == VK_F3) {
-        drawHandler = [this]() { drawSurnameBresenham(); };
+        Translate(0, -10);
     }
     else if (param == VK_F4) {
-        drawHandler = [this]() {
-            brush->SetOpacity(1.0f);
-            SetMainBrushColor(D2D1::ColorF::Green);
-            DDALine(1, 10, 1, 10);
-            SetMainBrushColor(D2D1::ColorF::Blue);
-            BresenhamLine(10, 20, 1, 10);
-
-            SetMainBrushColor(D2D1::ColorF::Red);
-            BresenhamCircle(20,20,10);
-            SetMainBrushColor(D2D1::ColorF::Purple);
-            WuLine(20, 30, 1, 10);
-           
-        };
+        Translate(0, 10);
+    }
+    else if (param == VK_F5) {
+        Translate(-10, 0);
+    }
+    else if (param == VK_F6) {
+        Translate(10, 0);
+    }
+    else if (param == VK_F7) {
+        xMirror();
+    }
+    else if (param == VK_F8) {
+        yMirror();
     }
 }
 
-void render::BresenhamLine(int x1, int x2, int y1, int y2)
-{
-    int dx = abs(x2 - x1);
-    int dy = -abs(y2 - y1);
-    int sx = (x1 < x2) ? 1 : -1;
-    int sy = (y1 < y2) ? 1 : -1;
-    int error = dx + dy;
-    while (true) {
-        rtv->FillRectangle(pixel(x1, y1), brush);
-        if (x1 == x2 && y1 == y2) break;
-        int e2 = 2 * error;
-        if (e2 >= dy) {
-            error += dy;
-            x1 += sx;
-        }
-        if (e2 <= dx) {
-            error += dx;
-            y1 += sy;
-        }
+D2D1_POINT_2F render::ComputePoint(float t){
+    int n = controlPoints.size() - 1;
+    D2D1_POINT_2F result = { 0,0 };
+    for (int i = 0; i <= n; i++) {
+        float coeff = BinomialCoefficient(n, i) * pow(1 - t, n - i) * pow(t, i);
+        result.x += coeff * controlPoints[i].x;
+        result.y += coeff * controlPoints[i].y;
+    }
+    return result;
+}
+
+void render::Rotate(float angle) {
+    D2D1_POINT_2F center{ 0, 0 };
+    for (const auto& point : controlPoints) {
+        center.x += point.x;
+        center.y += point.y;
+    }
+    center.x /= static_cast<float>(controlPoints.size());
+    center.y /= static_cast<float>(controlPoints.size());
+
+    for (auto& point : controlPoints) {
+        float x = point.x - center.x;
+        float y = point.y - center.y;
+        float xNew = x * cos(angle) - y * sin(angle);
+        float yNew = x * sin(angle) + y * cos(angle);
+        point.x = xNew + center.x;
+        point.y = yNew + center.y;
     }
 }
 
-void render::BresenhamCircle(int xc, int yc, int r)
-{
-    int x = r;
-    int y = 0;
-    int radiusError = 1 - x;
-    while (x >= y) {
-        rtv->FillRectangle(pixel(xc + x, yc - y), brush);
-        rtv->FillRectangle(pixel(xc - x, yc - y), brush);
-        rtv->FillRectangle(pixel(xc + y, yc - x), brush);
-        rtv->FillRectangle(pixel(xc - y, yc - x), brush);
-        if (x != y) {
-            rtv->FillRectangle(pixel(xc + y, yc + x), brush);
-            rtv->FillRectangle(pixel(xc - y, yc + x), brush);
-            rtv->FillRectangle(pixel(xc + x, yc + y), brush);
-            rtv->FillRectangle(pixel(xc - x, yc + y), brush);
-        }
-        y++;
-        if (radiusError < 0) {
-            radiusError += 2 * y + 1;
-        }
-        else {
-            x--;
-            radiusError += 2 * (y - x + 1);
-        }
+void render::Scale(float scale) {
+    D2D1_POINT_2F center = { 0, 0 };
+    for (const auto& point : controlPoints) {
+        center.x += point.x;
+        center.y += point.y;
+    }
+    center.x /= static_cast<float>(controlPoints.size());
+    center.y /= static_cast<float>(controlPoints.size());
+
+    for (auto& point : controlPoints) {
+        point.x *= scale;
+        point.y *= scale;
     }
 }
 
-void render::DDALine(float x1, float x2, float y1, float y2)
-{
-    float dx = x2 - x1;
-    float dy = y2 - y1;
-    int steps = max(abs(dx), abs(dy));
-    float xIncrement = dx / steps;
-    float yIncrement = dy / steps;
-    for (int i = 0; i <= steps; i++) {
-        int x = round(x1 + i * xIncrement);
-        int y = round(y1 + i * yIncrement);
-        rtv->FillRectangle(pixel(x, y), brush);
+void render::Translate(float dx, float dy) {
+    for (auto& point : controlPoints) {
+        point.x += dx;
+        point.y += dy;
     }
 }
 
-void render::WuLine(int x1, int x2, int y1, int y2)
-{
-    bool isSteep = abs(y2 - y1) > abs(x2 - x1);
-    if (isSteep) {
-        std::swap(x1, y1);
-        std::swap(x2, y2);
-    }
-    if (x1 > x2) {
-        std::swap(x1, x2);
-        std::swap(y1, y2);
-    }
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-    float gradient = static_cast<float>(dy) / static_cast<float>(dx);
-    float intery = y1 + gradient;
-    for (int x = x1; x <= x2; x++) {
-        float intensity1 =(1 - intery + floor(intery));
-        float intensity2 =(intery - floor(intery));
-        if (isSteep) {
-            brush->SetOpacity(intensity1);
-            rtv->FillRectangle(pixel(floor(intery), x), brush);
-            brush->SetOpacity(intensity2);
-            rtv->FillRectangle(pixel(ceil(intery), x), brush);
-        }
-        else {
-            brush->SetOpacity(intensity1);
-            rtv->FillRectangle(pixel(x, floor(intery)), brush); 
-            brush->SetOpacity(intensity2);
-            rtv->FillRectangle(pixel(x, ceil(intery)), brush);
-        }
-        intery += gradient;
+void render::xMirror() {
+    for (auto& point : controlPoints) {
+        point.y = WNDSettings().height - point.y;
     }
 }
 
-void render::drawSurnameBresenham()
-{
-    brush->SetOpacity(1.0f);
-    GetTimer()->Reset();
-    GetTimer()->Start();
-    // S
-    BresenhamLine(2, 20, 2, 2);
-    BresenhamLine(2, 2, 2, 10);
-    BresenhamLine(2, 20, 10, 10);
-    BresenhamLine(20, 20, 10, 20);
-    BresenhamLine(2, 20, 20, 20);
-    //// A
-    BresenhamLine(25, 30, 20, 2);
-    BresenhamLine(30, 40, 2, 20);
-    BresenhamLine(25, 35, 9, 9);
-    //// M
-    BresenhamLine(40, 40, 20, 2);
-    BresenhamLine(40, 45, 2, 10);
-    BresenhamLine(45, 50, 10, 2);
-    BresenhamLine(50, 50, 2, 20);
-    //// O
-    BresenhamCircle(61, 11, 9);
-    //// I
-    BresenhamLine(73, 73, 20, 2);
-    BresenhamLine(71, 75, 2, 2);
-    BresenhamLine(71, 75, 20, 20);
-    //// L
-    BresenhamLine(77, 77, 20, 2);
-    BresenhamLine(77, 85, 20, 20);
-    //// E
-    BresenhamLine(87,87,20,2);
-    BresenhamLine(87,93,20,20);
-    BresenhamLine(87,93,11,11);
-    BresenhamLine(87,93,2,2);
-    //// N
-    BresenhamLine(95, 95, 2, 20);
-    BresenhamLine(95, 103, 2, 20);
-    BresenhamLine(103, 103, 2, 20);
-    //// K
-    BresenhamLine(105, 105, 2, 20);
-    BresenhamLine(105, 110, 11,2);
-    BresenhamLine(105, 110, 11,20);
-    //// O
-    BresenhamCircle(120, 11, 9);
-
-    GetTimer()->Stop();
-
-    auto time = GetTimer()->GetTime();
-    std::cout << "TIME: " << time << std::endl;
+void render::yMirror() {
+    for (auto& point : controlPoints) {
+        point.x = WNDSettings().width - point.x;
+    }
 }
 
-void render::drawSurnameDDA()
-{
-    brush->SetOpacity(1.0f);
-    GetTimer()->Reset();
-    GetTimer()->Start();
-    // S
-    DDALine(2, 20, 2, 2);
-    DDALine(2, 2, 2, 10);
-    DDALine(2, 20, 10, 10);
-    DDALine(20, 20, 10, 20);
-    DDALine(2, 20, 20, 20);
-    //// A
-    DDALine(25, 30, 20, 2);
-    DDALine(30, 40, 2, 20);
-    DDALine(25, 35, 9, 9);
-    //// M
-    DDALine(40, 40, 20, 2);
-    DDALine(40, 45, 2, 10);
-    DDALine(45, 50, 10, 2);
-    DDALine(50, 50, 2, 20);
-    //// O
-    DDALine(52, 52, 20, 2);
-    DDALine(52, 68, 20, 20);
-    DDALine(68, 68, 20, 2);
-    DDALine(68, 52, 2, 2);
-    //// I
-    DDALine(73, 73, 20, 2);
-    DDALine(71, 75, 2, 2);
-    DDALine(71, 75, 20, 20);
-    //// L
-    DDALine(77, 77, 20, 2);
-    DDALine(77, 85, 20, 20);
-    //// E
-    DDALine(87, 87, 20, 2);
-    DDALine(87, 93, 20, 20);
-    DDALine(87, 93, 11, 11);
-    DDALine(87, 93, 2, 2);
-    //// N
-    DDALine(95, 95, 2, 20);
-    DDALine(95, 103, 2, 20);
-    DDALine(103, 103, 2, 20);
-    //// K
-    DDALine(105, 105, 2, 20);
-    DDALine(105, 110, 11, 2);
-    DDALine(105, 110, 11, 20);
-    //// O
-    DDALine(112, 112, 20, 2);
-    DDALine(112, 127, 20, 20);
-    DDALine(127, 127, 20, 2);
-    DDALine(127, 112, 2, 2);
-
-    GetTimer()->Stop();
-
-    auto time = GetTimer()->GetTime();
-    std::cout << "TIME: " << time << std::endl;
-}
-
-void render::drawSurnameWu()
-{
-    brush->SetOpacity(1.0f);
-    GetTimer()->Reset();
-    GetTimer()->Start();
-
-    // S
-    WuLine(2, 20, 2, 2);
-    WuLine(2, 2, 2, 10);
-    WuLine(2, 20, 10, 10);
-    WuLine(20, 20, 10, 20);
-    WuLine(2, 20, 20, 20);
-    //// A
-    WuLine(25, 30, 20, 2);
-    WuLine(30, 40, 2, 20);
-    WuLine(25, 35, 9, 9);
-    //// M
-    WuLine(40, 40, 20, 2);
-    WuLine(40, 45, 2, 10);
-    WuLine(45, 50, 10, 2);
-    WuLine(50, 50, 2, 20);
-    //// O
-    WuLine(52, 52, 20, 2);
-    WuLine(52, 68, 20, 20);
-    WuLine(68, 68, 20, 2);
-    WuLine(68, 52, 2, 2);
-    //// I
-    WuLine(73, 73, 20, 2);
-    WuLine(71, 75, 2, 2);
-    WuLine(71, 75, 20, 20);
-    //// L
-    WuLine(77, 77, 20, 2);
-    WuLine(77, 85, 20, 20);
-    //// E
-    WuLine(87, 87, 20, 2);
-    WuLine(87, 93, 20, 20);
-    WuLine(87, 93, 11, 11);
-    WuLine(87, 93, 2, 2);
-    //// N
-    WuLine(95, 95, 2, 20);
-    WuLine(95, 103, 2, 20);
-    WuLine(103, 103, 2, 20);
-    //// K
-    WuLine(105, 105, 2, 20);
-    WuLine(105, 110, 11, 2);
-    WuLine(105, 110, 11, 20);
-    //// O
-    WuLine(112, 112, 20, 2);
-    WuLine(112, 127, 20, 20);
-    WuLine(127, 127, 20, 2);
-    WuLine(127, 112, 2, 2);
-
-
-    GetTimer()->Stop();
-
-    auto time = GetTimer()->GetTime();
-    std::cout << "TIME: " << time << std::endl;
+int render::BinomialCoefficient(int n, int k) {
+    int result = 1;
+    for (int i = 1; i <= k; i++) {
+        result *= (n - i + 1);
+        result /= i;
+    }
+    return result;
 }
--- lab1_CG_Samoilenko/render.h
+++ lab1_CG_Samoilenko/render.h
@@ -2,19 +2,6 @@
 #define _DISABLE_FDW_MACROSES
 #include <D2DFWStandalone.h>
 
-#define size 5
-
-struct pixel : public D2D1_RECT_F {
-	pixel(int x, int y) {
-		x *= size;
-		y *= size;
-		left = x; 
-		right = x + size;
-		top = y;
-		bottom = y + size;
-	}
-};
-
 class render :
 	public FD2DW::D2DFWStandalone
 {
@@ -28,22 +15,31 @@ public:
 	virtual void UserD2DClose() override;
 	virtual void ChildKeyPressed(WPARAM) override;
 public:
-	//Bresenham
-	void BresenhamLine(int x1, int x2, int y1, int y2);
-	void BresenhamCircle(int xc, int yc, int r);
-	//DDA
-	void DDALine(float x1, float x2, float y1, float y2);
-	//Wu
-	void WuLine(int x1, int x2, int y1, int y2);
 
-	void drawSurnameBresenham();
-	void drawSurnameDDA();
-	void drawSurnameWu();
 protected:
 	ID2D1HwndRenderTarget* rtv;
 	ID2D1SolidColorBrush* brush;
 
-	std::function<void(void)> drawHandler;
+    std::vector<D2D1_POINT_2F> controlPoints = {
+        {100, 200},
+        {1000, 300},
+        {105, 247},
+        {159, 533},
+        {486, -185},
+        {415, 352}
+    };
 
+    float ScaleFactor = 1.0f;
+
+    ////////////
+    // LAB 2 METHODS
+    D2D1_POINT_2F ComputePoint(float t);
+    void Rotate(float angle);
+    void Scale(float scale);
+    void Translate(float dx, float dy);
+    void xMirror();
+    void yMirror();
+
+private:
+    int BinomialCoefficient(int n, int k);
 };
-
